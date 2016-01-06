82621575b7c812e18a3983eb1d79ab697ec58a73
RitwikGopi <ritwikgopi@gmail.com>
2016-01-06 19:11:10 +0530

    FINAL

---
+++ Scilab2C/PART_B/4_b/README.md
@@ -0,0 +1,7 @@
+To run this program use hardware with 1MHz internal clock 
+
+Settings for uart
+
+4800 Baud
+no parity
+1 stop bit
--- Scilab2C/PART_B/5/5.c
+++ Scilab2C/PART_B/5/5.c
@@ -2,9 +2,11 @@
 #define F_CPU 8000000
 
 main(){
-	DDRD |= (1 << 5);
+	DDRD |= (1 << 5);//OCR1A is made as output
 	TCCR1A |= (1 << COM1A1) | (1 << WGM11);
 	TCCR1B |= (1 << WGM13) | (1 << WGM12) | (1 << CS11);
+	//above two lines are set FAST PWM mode with clear on compare match
+	//prescaler is set to 64
 	ICR1 = 499;
 	OCR1A = 250;
 	while(1);
